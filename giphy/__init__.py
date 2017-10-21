a53e9d139052d5abff9e3f5a9096cab115260449
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-10-21 21:33:55 +0300

    init commit

---
+++ giphy/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'Mike Yusko'
+__version__ = '0.1.0'
---
+++ tests/test_giphy.py
@@ -0,0 +1,16 @@
+"""File for tests"""
+import unittest
+
+
+class TestGiphy(unittest.TestCase):
+    """Test class for the Giphy client class"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
