cc87a84b39fda42f1564fc2734118a8c6fdc122f
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-10-21 22:21:19 +0300

    Fix travis-ci builds

--- giphy/exceptions.py
+++ giphy/exceptions.py
@@ -5,7 +5,7 @@ BASE_TOKEN_ERROR_MSG = 'Api key must provided'
 
 class GiphyTokenError(Exception):
     """Base class for token errors"""
-    def __init__(self, msg=BASE_TOKEN_ERROR_MSG): # pylint: disable=super-init-not-called
+    def __init__(self, msg=BASE_TOKEN_ERROR_MSG):  # pylint: disable=super-init-not-called
         self.msg = msg
 
     def __str__(self):
