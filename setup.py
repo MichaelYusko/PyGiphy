47c1f8e264fc35b7ae63d3c01e32c5645fbba09f
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-10-24 20:54:16 +0300

    Previous api key, is not valid;)

--- setup.py
+++ setup.py
@@ -1,36 +1,32 @@
-# from distutils.core import setup
-#
-# import giphy
-#
-# VERSION = giphy.__version__
-# AUTHOR = giphy.__author__
-#
-# setup_kwargs = {
-#     'name': 'giphy',
-#     'version': VERSION,
-#     'url': 'https://github.com/MichaelYusko/PyGiphy',
-#     'license': 'MIT',
-#     'author': AUTHOR,
-#     'author_email': 'freshjelly12@yahoo.com',
-#     'description': 'Python interface for the Giphy API',
-#     'packages': ['giphy'],
-#     'classifiers': [
-#         'Development Status :: 2 - Pre-Alpha',
-#         'Environment :: Web Environment',
-#         'Programming Language :: Python :: 3.5',
-#         'Programming Language :: Python :: 3.6',
-#         'License :: OSI Approved :: MIT License'
-#     ],
-#  }
-#
-# requirements = ['requests>=2.13.0']
-# setup_kwargs['install_requires'] = requirements
-#
-# setup(**setup_kwargs)
-#
-# print(u"\n\n\t\t    "
-#       "PyGiphy version {} installation succeeded.\n".format(VERSION))
+from distutils.core import setup
 
-from giphy.client import GiphyClient
+import giphy
 
-print(GiphyClient('e4bcf0c50d5e45a79ed8a8d5503a065c').translate.gifs('homer simpson'))
+VERSION = giphy.__version__
+AUTHOR = giphy.__author__
+
+setup_kwargs = {
+    'name': 'giphy',
+    'version': VERSION,
+    'url': 'https://github.com/MichaelYusko/PyGiphy',
+    'license': 'MIT',
+    'author': AUTHOR,
+    'author_email': 'freshjelly12@yahoo.com',
+    'description': 'Python interface for the Giphy API',
+    'packages': ['giphy'],
+    'classifiers': [
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License'
+    ],
+ }
+
+requirements = ['requests>=2.13.0']
+setup_kwargs['install_requires'] = requirements
+
+setup(**setup_kwargs)
+
+print(u"\n\n\t\t    "
+      "PyGiphy version {} installation succeeded.\n".format(VERSION))
